07637a1dc03e368274dbf70684e73bd890f659f1
sandrasaji12 <144683732+sandrasaji12@users.noreply.github.com>
2024-08-12 11:25:50 +0530

    Add files via upload

---
+++ 1darray.py
@@ -0,0 +1,13 @@
+print("SJC23MCA-2047 : SANDRA SAJI")
+print("Batch : MCA 2023-25")
+
+import numpy as np
+one_dimensional_array = np.arange(10)
+first_4_elements = one_dimensional_array[:4]
+last_6_elements = one_dimensional_array[-6:]
+elements_2_to_7 = one_dimensional_array[2:8]
+
+print("Original array:",one_dimensional_array)
+print("a. First 4 elements:",first_4_elements)
+print("b. Last 6 elements:",last_6_elements)
+print("c. Elements from index 2to 7:",elements_2_to_7)
