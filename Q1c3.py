fb9b063a585e1cf6be70848ba182d9c7cd142276
sandrasaji12 <144683732+sandrasaji12@users.noreply.github.com>
2024-10-16 15:07:05 +0530

    Add files via upload

---
+++ Q1c3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+years = [2001, 2002, 2003, 2004, 2005, 2006, 2007]
+car_values = [24000, 22500, 19700, 17500, 14500, 10000, 5800]
+plt.figure(figsize=(10, 6))
+plt.subplot(111)
+plt.plot(years, car_values, linestyle='-.', color='red', marker='*',markersize=20, markerfacecolor='green')
+plt.title("SANDRA SAJI \nMCA 2023-2025", loc="right")
+plt.title("Value depreciation", loc="left")
+plt.xlabel("Year")
+plt.ylabel("Car value")
+
+plt.show()
